2c26c40ff3eb986441780c60c9c6a0022ac37b27
nandazman <nandazmann@gmail.com>
2022-04-22 09:34:15 +0700

    move to src folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
-import Layout from '../components/layout'
+import Layout from '../components/layout';
 
 export default function Home() {
+  var a = "123";
+  console.log("a")
   return (
     <Layout>
       <div>
