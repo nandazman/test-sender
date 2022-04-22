b12b00727ab81523675743ec33745d26c14a0122
nandazman <nandazmann@gmail.com>
2022-04-22 08:53:20 +0700

    add tailwind css and scss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
   reactStrictMode: true,
-}
+};
 
 module.exports = nextConfig
