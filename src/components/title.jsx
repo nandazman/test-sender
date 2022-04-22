eef9a3dc1858a224d03729bc43142e6dc486f7b4
nandazman <nandazmann@gmail.com>
2022-04-22 16:28:40 +0700

    add descriptiopn on title

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,6 +1,6 @@
 export default function Title({ text, children, description }) {
   return (
-    <div>
+    <div className="mb-32px">
       <div className="flex items-center gap-x-16px mb-5px">
         <span>{children}</span>
         <h4 className="text-h4 font-semibold text-dark-blue">{text}</h4>
